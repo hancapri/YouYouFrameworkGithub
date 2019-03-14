3a70662a7e3436892d6893b8733f4465c946c911
hancapri <1090963987@qq.com>
2019-03-14 08:04:12 +0800

    测试多线程异步
    1.测试多线程异步

--- Assets/YouYouFramework/Test/TestAsync.cs
+++ Assets/YouYouFramework/Test/TestAsync.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
+using System.Threading.Tasks;
 
 public class TestAsync : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Task.Factory.StartNew(DebugMore);
+        //DebugMore();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void DebugMore()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            Debug.Log(i);
+        }
+    }
 }

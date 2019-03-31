b7858f5c74cee1d5e97233bc68275c0cda1847f4
hancapri <1090963987@qq.com>
2019-03-31 20:31:48 +0800

    编写UI组件
    1.编写UI组件

--- Assets/YouYouFramework/Components/BaseComponent/UIComponent.cs
+++ Assets/YouYouFramework/Components/BaseComponent/UIComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace YouYouFramework
 {
@@ -10,11 +11,50 @@ namespace YouYouFramework
     /// </summary>
     public class UIComponent : YouYouBaseComponent, IUpdateComponent
     {
+        [Header("标准分辨率宽度")]
+        [SerializeField]
+        private int m_StandardWidth = 1280;
+
+        [Header("标准分辨率高度")]
+        [SerializeField]
+        private int m_StandardHeight = 720;
+
+        [Header("UI摄像机")]
+        public Camera UICamera;
+
+        [Header("根画布的缩放")]
+        [SerializeField]
+        private CanvasScaler m_UIRootCanvasScaler;
         protected override void OnAwake()
         {
             base.OnAwake();
             GameEntry.RegisterUpdateComponent(this);
-        }        
+        }
+
+        /// <summary>
+        /// 自动缩放
+        /// </summary>
+        public void AutoCanvasScaler()
+        {
+            float standard = m_StandardWidth / (float)m_StandardHeight;
+            float currScreen = Screen.width / (float)Screen.height;
+            if (currScreen > standard)
+            {
+                m_UIRootCanvasScaler.matchWidthOrHeight = 0;
+            }
+            else
+            {
+                m_UIRootCanvasScaler.matchWidthOrHeight = standard - currScreen;
+            }
+        }
+
+        /// <summary>
+        /// 设置缩放为1
+        /// </summary>
+        public void SetCanvasScaler()
+        {
+            m_UIRootCanvasScaler.matchWidthOrHeight = 1;
+        }
 
         public void OnUpdate()
         {
@@ -23,7 +63,7 @@ namespace YouYouFramework
 
         public override void Shutdown()
         {
-            
+
         }
     }
 }

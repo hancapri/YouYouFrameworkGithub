6fb1a342a7c2eb2c35fc08a5f115386350cee46b
hancapri <1090963987@qq.com>
2019-04-03 15:00:37 +0800

    更新UI资源4-3-15-00
    1.更新UI资源

---
+++ Assets/YouYouScripts/UI/UIForm/UITaskForm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YouYouFramework;
+
+public class UITaskForm : UIFormBase {
+
+    protected override void OnInit(object userData)
+    {
+        base.OnInit(userData);
+        Debug.Log("OnInit");
+    }
+    protected override void OnOpen(object userData)
+    {
+        base.OnOpen(userData);
+        Debug.Log("OnOpen");
+    }
+    protected override void OnClose()
+    {
+        base.OnClose();
+        Debug.Log("OnClose");
+    }
+    protected override void OnBeforeDestroy()
+    {
+        base.OnBeforeDestroy();
+        Debug.Log("OnBeforeDestroy");
+    }
+}

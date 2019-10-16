e4ec9e423cf9965dfa99c6f5a4a3ae305ed5ed06
hancapri <1090963987@qq.com>
2019-10-16 14:24:42 +0800

    1.上传LuaForm
    1.上传LuaForm

---
+++ Assets/YouYouFramework/Managers/Lua/LuaForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace YouYouFramework
+{
+    /// <summary>
+    /// Lua窗口
+    /// </summary>
+    [LuaCallCSharp]
+    public class LuaForm : UIFormBase
+    {
+        [CSharpCallLua]
+        public delegate void OnInitHandler(object userData);
+        OnInitHandler onInit;
+
+        [CSharpCallLua]
+        public delegate void OnOpenHandler(object userData);
+        OnOpenHandler onOpen;
+
+        [CSharpCallLua]
+        public delegate void OnCloseHandler();
+        OnCloseHandler onClose;
+
+        [CSharpCallLua]
+        public delegate void OnBeforeDestroyHandler();
+        OnBeforeDestroyHandler onBeforeDestroy;
+
+        private LuaTable scriptEnv;
+        private LuaEnv luaEnv;
+
+        protected override void OnInit(object userData)
+        {
+            base.OnInit(userData);
+
+            luaEnv = LuaManager.luaEnv; //此处要从LuaManager上获取 全局只有一个
+
+            scriptEnv = luaEnv.NewTable();
+
+            LuaTable meta = luaEnv.NewTable();
+            meta.Set("__index", luaEnv.Global);
+            scriptEnv.SetMetaTable(meta);
+            meta.Dispose();
+
+            string prefabName = name;
+            if (prefabName.Contains("(Clone)"))
+            {
+                prefabName = prefabName.Split(new string[] { "(Clone)" }, StringSplitOptions.RemoveEmptyEntries)[0] + "View";
+            }
+
+            onInit = scriptEnv.GetInPath<OnInitHandler>(prefabName + ".OnInit");
+            onOpen = scriptEnv.GetInPath<OnOpenHandler>(prefabName + ".OnOpen");
+            onClose = scriptEnv.GetInPath<OnCloseHandler>(prefabName + ".OnClose");
+            onBeforeDestroy = scriptEnv.GetInPath<OnBeforeDestroyHandler>(prefabName + ".OnBeforeDestroy");
+
+            scriptEnv.Set("self", this);
+            if (onInit != null)
+            {
+                onInit(userData);
+            }
+        }
+
+        protected override void OnOpen(object userData)
+        {
+            base.OnOpen(userData);
+            if (onOpen != null)
+            {
+                onOpen(userData);
+            }
+        }
+
+        protected override void OnClose()
+        {
+            base.OnClose();
+            if (onClose != null)
+            {
+                onClose();
+            }
+        }
+
+        protected override void OnBeforeDestroy()
+        {
+            base.OnBeforeDestroy();
+            if (onBeforeDestroy != null)
+            {
+                onBeforeDestroy();
+            }
+            onInit = null;
+            onOpen = null;
+            onClose = null;
+            onBeforeDestroy = null;
+        }
+    }
+}

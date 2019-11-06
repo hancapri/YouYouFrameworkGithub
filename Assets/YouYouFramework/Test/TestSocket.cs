daaef796b45783e7870b7f8e517811e80c16adde
hancapri <1090963987@qq.com>
2019-11-06 23:38:34 +0800

    1.测试新工程工具
    1.测试新工程工具

--- Assets/YouYouFramework/Test/TestSocket.cs
+++ Assets/YouYouFramework/Test/TestSocket.cs
@@ -16,7 +16,7 @@ public class TestSocket : MonoBehaviour {
         //sc.OnUpdate();
         if (Input.GetKeyDown(KeyCode.S))
         {
-            GameEntry.Socket.ConnectToMainSocket("192.168.0.106", 1039);
+            GameEntry.Socket.ConnectToMainSocket("192.168.0.108", 1039);
         }
 
         if (Input.GetKeyDown(KeyCode.T))
